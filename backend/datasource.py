c44ec57abe34a9578e0ed959c563e26c17bdfe3e
yuezjames <55596916+yuezjames@users.noreply.github.com>
2019-10-25 22:56:25 -0400

    Update datasource.py

--- backend/datasource.py
+++ backend/datasource.py
@@ -124,6 +124,8 @@ def main():
 	
 	# get list of movies in results
 	results = datasource.getMoviesByLength(connection, 60, 90)
+	# results = datasource.getMoviesByGenre(connection, 'Horror')
+	# results = datasource.getMoviesByVoteAverage(connection, 8.2)
 	
 	if results is not None:
 		print("Query results: ")
