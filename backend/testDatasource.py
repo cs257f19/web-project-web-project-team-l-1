230c142d34fba681af58fa2e4a6de4227e4f8bdc
yuezjames <55596916+yuezjames@users.noreply.github.com>
2019-10-30 23:19:15 -0400

    Update testDatasource.py

--- backend/testDatasource.py
+++ backend/testDatasource.py
@@ -4,18 +4,38 @@ from datasource import *
 class DataSourceTester(unittest.TestCase):
     
     def setUp(self):
+	
+	'''
+	Set up the connection to database by providing username and password.
+	'''
+		
 	self.movie = DataSource('yuez','glass944happy')
 	self.connection = self.movie.tryConnect()
 
     def test_getDirectorByMovie_correct(self):
+	
+	'''
+	Test if the method returns correct director name of a given movie.
+	'''
+	
         title = 'Avatar'
         self.assertEqual(self.movie.getDirectorByMovie(self.connection, title), "James Cameron")
 	
     def test_getDirectorByMovie_empty(self):
+	
+	'''
+	Test if the method returns empty string instead of director name given name of movie.
+	'''
+	
         title = 'Avatar'
         self.assertNotEqual(self.movie.getDirectorByMovie(self.connection, title), "")
 	
     def test_getDirectorByMovie_type(self):
+	
+	'''
+	Test if the method returns director name of a given movie in type of string.
+	'''
+	
 	title = 'Avatar'
 	string = ''
 	self.assertEqual(type(self.movie.getDirectorByMovie(self.connection, title)), type(string))
