17eaa21b77a50dc7bac4a3751807c1469d2d3992
ChrisKitchen <chriskitchenrocks@gmail.com>
2019-10-30 21:34:03 -0500

    Update testDatasource.py

--- backend/testDatasource.py
+++ backend/testDatasource.py
@@ -3,7 +3,7 @@ from datasource import *
 
 class DataSourceTester(unittest.TestCase):
     
-    def setUp(self) --> None:
+    def setUp(self):
 	self.connection = DataSource('yuez','glass944happy').tryConnect()
 
     def test_getDirectorByMovie(self):
