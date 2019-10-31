53325637095a6b98cd523cb59ff726b8668627d4
ChrisKitchen <chriskitchenrocks@gmail.com>
2019-10-30 21:35:39 -0500

    Update testDatasource.py

--- backend/testDatasource.py
+++ backend/testDatasource.py
@@ -8,7 +8,7 @@ class DataSourceTester(unittest.TestCase):
 
     def test_getDirectorByMovie(self):
         title = 'Avatar'
-        self.assertEqual(self.movie.getDirectorByMovie(self.connection, title), "James Cameron")
+        self.assertEqual(self.getDirectorByMovie(self.connection, title), "James Cameron")
 
 if __name__ == "__main__":
 	unittest.main()
