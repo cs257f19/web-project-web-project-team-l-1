6ea48d5332cc15ad1e9c56d47dcdfdb4e21cef4e
yuezjames <55596916+yuezjames@users.noreply.github.com>
2019-10-30 23:05:53 -0400

    Update testDatasource.py

--- backend/testDatasource.py
+++ backend/testDatasource.py
@@ -7,13 +7,17 @@ class DataSourceTester(unittest.TestCase):
 	self.movie = DataSource('yuez','glass944happy')
 	self.connection = self.movie.tryConnect()
 
-    def test_getDirectorByMovie(self):
+    def test_getDirectorByMovie_correct(self):
         title = 'Avatar'
         self.assertEqual(self.movie.getDirectorByMovie(self.connection, title), "James Cameron")
 	
     def test_getDirectorByMovie_empty(self):
         title = 'Avatar'
         self.assertNotEqual(self.movie.getDirectorByMovie(self.connection, title), "")
+	
+    def test_getDirectorByMovie_type(self):
+	title = 'Avatar'
+	self.assertEqual(type(self.movie.getDirectorByMovie(self.connection, title)), 'str')
 	
 if __name__ == "__main__":
 	unittest.main()
