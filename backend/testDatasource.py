5d3b52ef071816bcbd39ebb1da5ff16912b908f3
yuezjames <55596916+yuezjames@users.noreply.github.com>
2019-10-30 22:09:44 -0400

    Update testDatasource.py

--- backend/testDatasource.py
+++ backend/testDatasource.py
@@ -1,1 +1,18 @@
+import unittest
+from datasource import *
 
+class DataSourceTester(unittest.TestCase):
+    user = 'yuez'
+    password = 'glass944happy'
+    datasource = Datasource(user, password)
+    connection = datasource.tryConnect()
+    
+    def setUp(self) -> None:
+        self.movie = DataSource()
+
+    def test_getDirectorByMovie(self):
+        title = 'Avatar'
+        self.assertEqual(self.movie.getDirectorByMovie(connection, title), "James Cameron")
+
+if __name__ == "__main__":
+	unittest.main()
