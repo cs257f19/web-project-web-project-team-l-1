14c3314f85e0d1dee67354355d399324df31b479
yuezjames <55596916+yuezjames@users.noreply.github.com>
2019-11-12 16:03:15 -0500

    Add files via upload

---
+++ testWeb/webapp.py
@@ -0,0 +1,19 @@
+import flask
+from flas import render_template
+import json
+import sys
+
+app = flask.Flask(_name_)
+
+@app.route('/')
+def displayHomepage():
+	return render_template('homepage1.html')
+	
+if _name_ == '_main_':
+	if len(sys.argv) != 3:
+		print('Usage: {0} host post'.format(sys.argv[0]), file = sys.stderr)
+		exit()
+		
+	host = sys.argv[1]
+	port = sys.argv[2]
+	app.run(host = host, port = port)
