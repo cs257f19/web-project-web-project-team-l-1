8318d274dc7fdfb99028d04868846ffb014a9251
ChrisKitchen <chriskitchenrocks@gmail.com>
2019-11-12 20:19:02 -0600

    Update webapp.py

--- testWeb/webapp.py
+++ testWeb/webapp.py
@@ -2,12 +2,26 @@ import flask
 from flask import render_template
 import json
 import sys
+sys.path.append('../')
+from datasource import *
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def displayHomepage():
 	return render_template('homepage1.html')
+
+@app.route('/fruit')
+def fruit():
+    myFruit = [
+        {'name': 'apple', 'rating': 7},
+        {'name': 'banana', 'rating': 5},
+        {'name': 'pear', 'rating': 4}
+    ]
+
+    return render_template('fruit.html',
+                           fruits=myFruit)
+
 	
 if __name__ == '__main__':
 	if len(sys.argv) != 3:
